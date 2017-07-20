ccf8f3206b9f31f24da353a68190e5480281914e
shubhram <das.shubhram.1981@gmail.com>
2017-07-20 19:58:16 +0530

    Object name Change

--- src/test/scala/com/hcl/frame/test/Main.scala
+++ src/test/scala/com/hcl/frame/test/Main.scala
@@ -2,7 +2,7 @@ package com.hcl.frame.test
 
 import com.hcl.frame.Frame
 
-object Test extends App {
+object Main extends App {
 
   val f: List[Frame] = Frame.getTotalStat(List(7, 2, 5, 5, 3, 0, 10, 2, 4, 10))
   println(f)
