a480dd9a82e8ca23dc37913ae0efce77d0a109fd
shubhram <das.shubhram.1981@gmail.com>
2017-07-20 19:54:50 +0530

    Initial commit

---
+++ src/main/scala/com/hcl/frame/Frame.scala
@@ -0,0 +1,43 @@
+package com.hcl.frame
+
+import scala.collection.mutable.ListBuffer
+
+case class Frame(hit1: Int, hit2: Int)
+
+object Frame {
+
+  val tmpBuffer = new ListBuffer[Int]
+
+  def getTotalStat(l: List[Int]): List[Frame] = {
+
+    val maxBowlHit = 10
+    val output: ListBuffer[Frame] = new ListBuffer[Frame]
+    l.foreach(x => {
+      if (x == maxBowlHit) {
+        output += Frame(x, 0)
+      }
+      else {
+        var frm = getFrame(x)
+        if (frm != null) {
+          output += frm
+        }
+      }
+    }
+    )
+    output.toList
+  }
+
+  def getFrame(b: Int): Frame = {
+    if (tmpBuffer.nonEmpty) {
+      val one = tmpBuffer.remove(0)
+      //println(tmpBuffer.nonEmpty)
+      Frame(one, b)
+
+    }
+    else {
+      tmpBuffer += b
+      null
+    }
+  }
+}
+
